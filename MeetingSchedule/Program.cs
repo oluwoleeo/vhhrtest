2de32e5012037bfa462513bf91291e1a3e832ac6
Oluwaremi Oluwole <polymathwole@gmail.com>
2020-02-16 17:04:42 +0100

    Add proj 2

---
+++ MeetingSchedule/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MeetingSchedule
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> arr = new List<int>() { 1, 1, 2 };
+            List<int> dep = new List<int>() { 1, 2, 2 };
+
+            Console.WriteLine(countMeetings(arr, dep));
+        }
+
+        public static int countMeetings(List<int> arrival, List<int> departure)
+        {
+            List<int> availables = new List<int>();
+
+            for(int i = 0; i < arrival.Count; i++)
+            {
+                if (arrival[i] == departure[i] && availables.IndexOf(arrival[i]) == -1)
+                {
+                    availables.Add(arrival[i]);
+                    continue;
+                }
+                else if (arrival[i] == departure[i] && availables.IndexOf(arrival[i]) > -1)
+                {
+                    continue;
+                }
+                
+                if (availables.IndexOf(arrival[i]) == -1)
+                {
+                    availables.Add(arrival[i]);
+                    /* if (arrival[i] > departure[i] && availables.IndexOf(departure[i]) == -1)
+                    {
+                        availables.Add(departure[i]);
+                    }
+                    else
+                    {
+                        availables.Add(arrival[i]);
+                    } */
+                }
+                else if (availables.IndexOf(departure[i]) == -1) {
+                    availables.Add(departure[i]);
+                }
+            }
+            return availables.Count;
+        }
+    }
+}
